12ea66c0269270336fa2833385bdf3291fd4921c
luke-codewalker <arathorn089@arcor.de>
2022-06-28 21:31:45 +0200

    basic main

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,8 @@
-fn main() {}
+use wordle;
+fn main() {
+    let mut game = wordle::Game::new(String::from("hello"));
+    match game.play(String::from("world")) {
+        Err(err) => eprintln!("{}", err),
+        Ok(history) => println!("{:?}", history),
+    };
+}
